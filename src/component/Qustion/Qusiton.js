ffcb0411f9e7be6a069ef0e3faf0e38cb3c9d775
Nahidfrahaman <nahi.frahaman@gmail.com>
2022-09-29 17:28:59 +0600

    qusiton section is done

--- src/component/Qustion/Qusiton.js
+++ src/component/Qustion/Qusiton.js
@@ -3,7 +3,7 @@ import './Qusiton.css';
 const Qusiton = () => {
     return (
         <div>
-            <div>
+            <div className=' bg-[#DFF6FF]'>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-[80%] mx-auto mt-5">
                <div className="collapse-title text-xl font-medium">
                   Qus:  How Does React work?
@@ -17,15 +17,15 @@ const Qusiton = () => {
                   Qus:  Differece between props and state??
                 </div>
                <div className="collapse-content"> 
-            <p>tabIndex={0} ReactJS divides the UI into isolated reusable pieces of code known as components. React compiled jsx laguage by babel, React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process  </p>
+            <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components </p>
            </div>
             </div>
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box w-[80%] mx-auto mt-5">
                <div className="collapse-title text-xl font-medium">
-                   How Does React work
+                  Qus: Example of Uses of useEffect without dataLoad?
                 </div>
                <div className="collapse-content"> 
-            <p>tabIndex={0} ReactJS divides the UI into isolated reusable pieces of code known as components. React compiled jsx laguage by babel, React goes through its virtual DOM ,creates a list of those changes that need to be made to the actual DOM and then does it all in one single process  </p>
+            <p>validating input field, live filtering, trigger animation on new array value, update paragraph list on fetched API data update  </p>
            </div>
             </div>
             </div>
